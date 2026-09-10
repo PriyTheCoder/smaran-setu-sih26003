d2377c9d877236158fe963c1f31c7cc4197709c7
Akash shukla <akashshukla7612@gmail.com>
2026-09-10 14:49:30 +0530

    Add new localization strings for Lus language

--- smaran-setu-frontend/src/locales/lus.js
+++ smaran-setu-frontend/src/locales/lus.js
@@ -25,7 +25,6 @@ const lus = {
 
   language: "Tawng",
   selectLanguage: "Tawng thlang rawh",
-
   notifications: "Hriattirna",
 
   goodMorning: "Tukchhuah nuam",
@@ -46,6 +45,55 @@ const lus = {
   submit: "Submit",
   back: "Kir leh",
   next: "Duh leh",
+
+  // Home page
+  playGames: "Infiamna te hi infiam rawh",
+  memoryActivities: "I hriatna tih chak zawk tur infiamna nuam te",
+  myMemories: "Ka hriatna te",
+  peopleMoments: "Ka hmangaihte leh hun hman ho te",
+  myRoutine: "Ka nitin tih tur te",
+  seeNext: "A dawttu en rawh",
+  myProgress: "Ka hma sawnna",
+  seeDoing: "Engtin nge ka tih en rawh",
+  open: "Hawn rawh",
+
+  welcomeSmaran: "Smaran Setu ah kan lo lawm e",
+  hello: "Chibai",
+  welcomeBack: "I hmun bikah kan lo lawm leh e.",
+  happyActive: "Tunah ni nuam leh chak tak kan siam dawn nia.",
+
+  doingGreat: "Tunah i ti tha hle!",
+  todaysWellness: "Tuni ni hriselna leh nun nuamna",
+  keepMindActive: "I rilru chu thawk zel rawh",
+
+  simpleActivities:
+    "Tih tur awlsam te chuan i ni chu nuam zawk leh hlim zawk an siam thei.",
+
+  activitiesDone: "Tih tur zawh te",
+  dailyGoal: "Nitin tum",
+
+  excellent: "Tha tak!",
+  almostThere: "A zawh tep tawh!",
+  keepGoing: "Kal zel rawh!",
+
+  yourSpace: "I hmun",
+  whatDo: "Eng nge i tih duh?",
+  chooseEnjoy: "I tih nuam zawng zawng zingah pakhat thlang rawh.",
+
+  nextActivity: "Tih tur dawt",
+  memoryMatch: "Memory Match",
+
+  memoryExercise:
+    "I hriatna tih chak zawk tur infiamna te nuam leh tlem.",
+
+  startActivity: "Tih tur tan rawh",
+
+  everyStep: "Tlangval te pawh a pawimawh",
+
+  takeYourTime:
+    "I hun la rawh, i tih tur te nuam takin ti la, ni tin hun hlimawm te siam zel rawh.",
+
+  needHelp: "Tanpuina i mamawh em? Smaran hnenah sawipui rawh",
 };
 
 export default lus;
