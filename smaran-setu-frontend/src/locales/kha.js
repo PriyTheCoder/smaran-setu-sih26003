abec92f8f146471998d97d088bc75106e37f93db
Akash shukla <akashshukla7612@gmail.com>
2026-09-10 14:48:19 +0530

    Update kha.js

--- smaran-setu-frontend/src/locales/kha.js
+++ smaran-setu-frontend/src/locales/kha.js
@@ -25,7 +25,6 @@ const kha = {
 
   language: "Ktien",
   selectLanguage: "Jied ïa ka ktien",
-
   notifications: "Ki jingpyntip",
 
   goodMorning: "Khublei mynstep",
@@ -46,6 +45,55 @@ const kha = {
   submit: "Submit",
   back: "Kylliang pat",
   next: "Bud",
+
+  // Home page
+  playGames: "Ïalehkai",
+  memoryActivities: "Ki jingïalehkai kiba pynbha ïa ka jingkynmaw jong phi",
+  myMemories: "Ki jingkynmaw jong nga",
+  peopleMoments: "Ki briew bad ki por kiba nga ieit",
+  myRoutine: "Ka rukom treikam jong nga",
+  seeNext: "Peit ïa kaba wan hadien",
+  myProgress: "Ka jingkiew jong nga",
+  seeDoing: "Peit kumno nga leh",
+  open: "Plie",
+
+  welcomeSmaran: "Khublei shibun sha Smaran Setu",
+  hello: "Khublei",
+  welcomeBack: "Khublei biang sha ka jaka kaba kyrpang jong phi.",
+  happyActive: "To pynlong ïa kane ka sngi kaba kmen bad kaba treikam.",
+
+  doingGreat: "Phi leh bha bha mynta!",
+  todaysWellness: "Ka koit ka khiah bad ka jingbha jong mynta",
+  keepMindActive: "Pynlong ïa ka jingmut jong phi kaba treikam",
+
+  simpleActivities:
+    "Ki katto katne ki kam kiba suk kin pynlong ïa ka sngi jong phi kaba kmen shuh shuh.",
+
+  activitiesDone: "Ki kam kiba la pyndep",
+  dailyGoal: "Ka thong jong ka sngi",
+
+  excellent: "Khublei shibun! Kaba bha tam!",
+  almostThere: "Lah jan dep!",
+  keepGoing: "Bteng ban leit shakhmat!",
+
+  yourSpace: "Ka jaka jong phi",
+  whatDo: "Phi kwah ban leh aiu?",
+  chooseEnjoy: "Jied ïa kaei kaba phi sngewtynnad ban leh.",
+
+  nextActivity: "Ka kam kaba wan hadien",
+  memoryMatch: "Memory Match",
+
+  memoryExercise:
+    "Ka kam kaba rit bad kaba kmen ban pynbha ïa ka jingkynmaw jong phi.",
+
+  startActivity: "Sdang ïa ka kam",
+
+  everyStep: "Man la kawei ka sienjam ka long kaba donkam",
+
+  takeYourTime:
+    "Shim ïa ka por jong phi, sngewbha ïa ki kam jong phi bad pynlong ïa man ka sngi kaba dap da ki jingkynmaw kiba itynnad.",
+
+  needHelp: "Donkam jingïarap? Iakren bad Smaran",
 };
 
 export default kha;
