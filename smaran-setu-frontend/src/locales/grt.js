8a189248ee35d2efe940fce8b159261a4ccac18c
Akash shukla <akashshukla7612@gmail.com>
2026-09-10 14:47:00 +0530

    Update grt.js

--- smaran-setu-frontend/src/locales/grt.js
+++ smaran-setu-frontend/src/locales/grt.js
@@ -25,7 +25,6 @@ const grt = {
 
   language: "Ku∙sik",
   selectLanguage: "Ku∙sik basebo",
-
   notifications: "Aganchakanirang",
 
   goodMorning: "Walasaning",
@@ -46,6 +45,55 @@ const grt = {
   submit: "Submit",
   back: "Back",
   next: "Next",
+
+  // Home page
+  playGames: "Game-rangko dakbo",
+  memoryActivities: "Nangni gisiko donaniko nambate dakna kusinani kamrang",
+  myMemories: "Angni niksengani",
+  peopleMoments: "Angna ka∙saanirang aro gisik ra∙ani somoirang",
+  myRoutine: "Angni salantina kamrang",
+  seeNext: "Gnigipa kamko nibo",
+  myProgress: "Angni batanggipa",
+  seeDoing: "Anga maikai dakenga uko nibo",
+  open: "Oe",
+
+  welcomeSmaran: "Smaran Setu-o na∙simangko salgri dongna",
+  hello: "Namaste",
+  welcomeBack: "Nangni nija biapo changsatai na∙simangko salgri dongna.",
+  happyActive: "Da∙o salrangko kusini aro kam ka∙ani salrang ong∙atna re∙baaha.",
+
+  doingGreat: "Na∙a da∙o namen namgipa kamko dakenga!",
+  todaysWellness: "Da∙alni namgipa aro walni janggi tangani",
+  keepMindActive: "Nangni gisikko kam ka∙atbo",
+
+  simpleActivities:
+    "Minggipin manggipa kamrang nangni salrangko aro kusinani ong∙atna dakchakgen.",
+
+  activitiesDone: "Kamrang matchotaha",
+  dailyGoal: "Salni miksongani",
+
+  excellent: "Namen namgipa!",
+  almostThere: "Matchotna sepangaha!",
+  keepGoing: "Doangmao aro kamko dakbo!",
+
+  yourSpace: "Nangni biap",
+  whatDo: "Na∙a maiko dakna nangnikenga?",
+  chooseEnjoy: "Na∙a kusinani dakna nangnikenggipa kamko basebo.",
+
+  nextActivity: "Gnigipa kam",
+  memoryMatch: "Memory Match",
+
+  memoryExercise:
+    "Nangni gisiko donaniko nambate dakna dakgipa fisa aro kusinani kam.",
+
+  startActivity: "Kamko a∙bachengatbo",
+
+  everyStep: "Minggipa fisa fisa ja∙kuaba nangchongmotgipa",
+
+  takeYourTime:
+    "Nangni somoiko ra∙bo, nangni kamrangko kusisibo aro salantina namgipa somoirangko dakna re∙mangmangbo.",
+
+  needHelp: "Dakchakani nangama? Smaran baksa agangrikaniko dakbo",
 };
 
 export default grt;
