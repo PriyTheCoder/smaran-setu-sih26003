23b62d10ce182884ac2209c70ea6c798f2bb4607
Akash shukla <akashshukla7612@gmail.com>
2026-09-10 14:50:42 +0530

    Add new localization strings for the Mni language

--- smaran-setu-frontend/src/locales/mni.js
+++ smaran-setu-frontend/src/locales/mni.js
@@ -25,7 +25,6 @@ const mni = {
 
   language: "ꯂꯣꯟ",
   selectLanguage: "ꯂꯣꯟ ꯈꯜꯂꯣ",
-
   notifications: "ꯅꯣꯇꯤꯐꯤꯀꯦꯁꯟ",
 
   goodMorning: "ꯅꯨꯃꯤꯠ ꯐꯖꯔꯦ",
@@ -46,6 +45,55 @@ const mni = {
   submit: "ꯊꯥꯕ",
   back: "ꯍꯟꯅ",
   next: "ꯃꯊꯪ",
+
+  // Home page
+  playGames: "ꯒꯦꯝꯁꯤꯡ ꯁꯣꯏꯕ",
+  memoryActivities: "ꯅꯪꯒꯤ ꯅꯨꯡꯁꯤꯇꯣꯡ ꯍꯦꯟꯅꯥ ꯐꯖꯕ ꯑꯣꯏꯅ ꯇꯧꯕ ꯅꯨꯡꯁꯤꯇꯣꯡꯒꯤ ꯂꯣꯟꯁꯤꯡ",
+  myMemories: "ꯑꯩꯒꯤ ꯅꯨꯡꯁꯤꯇꯣꯡ",
+  peopleMoments: "ꯑꯩꯅ ꯅꯨꯡꯉꯥꯏꯕ ꯃꯤꯁꯤꯡ ꯑꯃꯁꯨꯡ ꯃꯇꯝꯁꯤꯡ",
+  myRoutine: "ꯑꯩꯒꯤ ꯅꯤꯡꯊꯤꯕ ꯂꯣꯟꯁꯤꯡ",
+  seeNext: "ꯃꯊꯪ ꯇꯥꯕ ꯑꯗꯨ ꯌꯦꯡꯕ",
+  myProgress: "ꯑꯩꯒꯤ ꯃꯌꯥꯝ",
+  seeDoing: "ꯑꯩ ꯀꯔꯤ ꯇꯧꯔꯤꯕꯅꯣ ꯌꯦꯡꯕ",
+  open: "ꯍꯥꯡꯕ",
+
+  welcomeSmaran: "ꯁ꯭ꯃꯔꯟ ꯁꯦꯇꯨꯗ ꯇꯧꯔꯛꯄꯗ ꯅꯨꯡꯉꯥꯏ",
+  hello: "ꯍꯥꯌꯕ",
+  welcomeBack: "ꯅꯪꯒꯤ ꯃꯥꯁꯤꯒꯤ ꯃꯐꯝꯗ ꯑꯃꯨꯛ ꯇꯧꯔꯛꯄꯗ ꯅꯨꯡꯉꯥꯏ",
+  happyActive: "ꯍꯥꯌꯕ ꯅꯨꯃꯤꯠꯁꯤ ꯅꯨꯡꯉꯥꯏꯕ ꯑꯃꯁꯨꯡ ꯁꯛꯇꯝ ꯑꯣꯏꯅ ꯑꯣꯏꯁꯤꯜꯂꯨ",
+
+  doingGreat: "ꯅꯪ ꯉꯥꯏ ꯐꯖꯅꯥ ꯇꯧꯔꯤ!",
+  todaysWellness: "ꯉꯨꯁꯤꯒꯤ ꯁꯤꯕ ꯑꯃꯁꯨꯡ ꯐꯖꯕ ꯂꯩꯕ",
+  keepMindActive: "ꯅꯪꯒꯤ ꯅꯨꯡꯁꯤꯇꯣꯡ ꯁꯛꯇꯝ ꯑꯣꯏꯅ ꯂꯩꯍꯧ",
+
+  simpleActivities:
+    "ꯀꯌꯥꯃꯊꯣꯛ ꯑꯣꯏꯕ ꯂꯣꯟꯁꯤꯡꯅ ꯅꯪꯒꯤ ꯅꯨꯃꯤꯠꯁꯤ ꯍꯦꯟꯅꯥ ꯅꯨꯡꯉꯥꯏ ꯑꯣꯏꯍꯟꯕ ꯌꯥꯏ",
+
+  activitiesDone: "ꯃꯊꯪ ꯇꯧꯔꯕ ꯂꯣꯟꯁꯤꯡ",
+  dailyGoal: "ꯅꯤꯡꯊꯤꯕ ꯂꯥꯛꯄ",
+
+  excellent: "ꯐꯖꯅꯥ ꯇꯧꯔꯦ!",
+  almostThere: "ꯃꯊꯪ ꯇꯥꯕ ꯆꯥꯡꯂꯦ!",
+  keepGoing: "ꯆꯠꯊꯔꯛꯎ!",
+
+  yourSpace: "ꯅꯪꯒꯤ ꯃꯐꯝ",
+  whatDo: "ꯅꯪ ꯀꯔꯤ ꯇꯧꯕ ꯄꯥꯝꯕ?",
+  chooseEnjoy: "ꯅꯪꯅ ꯅꯨꯡꯉꯥꯏꯕ ꯂꯣꯟꯁꯤꯡ ꯑꯃ ꯈꯜꯂꯣ",
+
+  nextActivity: "ꯃꯊꯪ ꯇꯥꯕ ꯂꯣꯟ",
+  memoryMatch: "ꯃꯦꯃꯣꯔꯤ ꯃꯦꯆ",
+
+  memoryExercise:
+    "ꯅꯪꯒꯤ ꯅꯨꯡꯁꯤꯇꯣꯡ ꯍꯦꯟꯅꯥ ꯐꯖꯕ ꯑꯣꯏꯍꯟꯕ ꯅꯨꯡꯉꯥꯏꯕ ꯂꯣꯟ ꯑꯃ",
+
+  startActivity: "ꯂꯣꯟ ꯍꯧꯖꯤꯜꯂꯨ",
+
+  everyStep: "ꯈꯨꯠꯅꯥ ꯇꯧꯕ ꯆꯞꯆꯥ ꯑꯃ ꯑꯃ ꯃꯔꯨꯑꯣꯏ",
+
+  takeYourTime:
+    "ꯅꯪꯒꯤ ꯃꯇꯝ ꯂꯧꯕꯤꯌꯨ, ꯅꯪꯒꯤ ꯂꯣꯟꯁꯤꯡ ꯅꯨꯡꯉꯥꯏꯅꯥ ꯇꯧꯕꯤꯌꯨ ꯑꯃꯁꯨꯡ ꯅꯤꯡꯊꯤꯕ ꯐꯖꯕ ꯃꯇꯝꯁꯤꯡ ꯁꯦꯝꯕꯤꯌꯨ",
+
+  needHelp: "ꯃꯇꯦꯡ ꯇꯧꯕ ꯄꯥꯝꯕ? Smaranꯒ ꯂꯣꯟꯅ ꯋꯥꯔꯤ ꯁꯥꯅꯨ",
 };
 
 export default mni;
